c9d0159deffe18b5b48d2725b5f950ba84cb7330
Victor Fedorov <vpfedorov@bk.ru>
2021-12-23 19:14:49 +0300

    Initial Commit

---
+++ Shared/SwiftLSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftLSApp.swift
+//  Shared
+//
+//  Created by Ольга Федорова on 23.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftLSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
